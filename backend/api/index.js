782ce61ae1b396682fab5bb10dd3ff6b0f54d885
Umar Ishaq <umarishaq26@gmail.com>
2026-07-09 19:04:22 +0100

    fix node error

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,2 +1,4 @@
-const { default: app } = require("../dist/app");
+const path = require("path");
+require("ts-node").register({ transpileOnly: true, project: path.join(__dirname, "../tsconfig.json") });
+const app = require(path.join(__dirname, "../src/app")).default;
 module.exports = app;
